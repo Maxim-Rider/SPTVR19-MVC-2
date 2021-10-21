90c8c9c69650bf8ab818cf93592a6e6928c3deac
Maksim Kolesnikov <maksim.kolesnikov@ivkhk.ee>
2021-10-21 10:40:55 +0300

    MVC 2, база данных в массивах

---
+++ model/booksArray.php
@@ -0,0 +1,39 @@
+<?php 
+$books = array(
+    array(
+        "bookname" => "HTML and CSS",
+        "author" => "Jon Duckett",
+        "price" => "17.19$",
+        "description" => "Every day, more and more people want to learn some HTML and CSS. Joining the process.",
+        "image" => "HTMLandCSS_Duckett.jpg" 
+    ),
+    array(
+        "bookname" => "HTML5",
+        "author" => "Matthew MacDonald",
+        "price" => "27.65$",
+        "description" => "HTML5 is more than a markup language - it`s a collection of several independent web sites.",
+        "image" => "HTML5TheMissing_MacDonald.jpg" 
+    ),
+    array(
+        "bookname" => "Head First HTML5 Programming",
+        "author" => "Eric Freeman, Elisabeth Robson",
+        "price" => "35.35$",
+        "description" => "HTML has been on a wild ride. Sure, HTMl started as a mere markup language.",
+        "image" => "HeadFirstHTMl5.jpg" 
+    ),
+    array(
+        "bookname" => "Head First JavaScript Programming",
+        "author" => "Eric Freeman, Elisabeth Robson",
+        "price" => "37.60$",
+        "description" => "This brain-friendly guide teaches toy everything from JavaScript language. ",
+        "image" => "HeadFirstJavaScript.jpg" 
+    ),
+    array(
+        "bookname" => "jQuery in Action",
+        "author" => "Bear Bideault, Yehuda Katz",
+        "price" => "32.12$",
+        "description" => "A really good web development framework anticipates your needs. jQuery does more-it.",
+        "image" => "jQueryInAction.jpg" 
+    ),
+);
+?>
